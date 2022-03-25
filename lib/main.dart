76c786c00bec1b2c44768d66dd7adc67d4eb0c95
yodastf <yodastf@gmail.com>
2022-03-25 22:48:56 +0200

    commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:test_task_app/phone_number_validation_widget.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+ 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        
+        primarySwatch: Colors.blue,
+        
+      ),
+      home: const PhoneNumberValidationWidget(),
+    );
+  }
+}
---
+++ lib/phone_number_validation_widget.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class PhoneNumberValidationWidget extends StatefulWidget {
+  const PhoneNumberValidationWidget
+({Key? key}) : super(key: key);
+
+  @override
+  State<PhoneNumberValidationWidget> createState() => _PhoneNumberValidationWidgetState();
+  
+}
+
+class _PhoneNumberValidationWidgetState extends State<PhoneNumberValidationWidget> {
+   
+  final  _controller = TextEditingController();
+  int charLength = 0;
+  
+  void _clearTextField() {
+  
+    _controller.clear();
+  
+    setState(() {});
+  }
+  @override
+  Widget build(BuildContext context) {
+ 
+    return Scaffold(
+      body: Center(
+        child:Padding(
+          padding: const EdgeInsets.fromLTRB(15, 40, 15, 10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+      children:  [
+         const SizedBox(height: 70),
+      const Text("Get Started",
+      style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
+    
+      const SizedBox(height: 150),
+      TextFormField(
+        controller: _controller,
+        onChanged: (value) {
+          setState(() {
+            
+          });
+        },
+          decoration:  InputDecoration(
+            hintText: '(201) 555-0123',
+            helperText: 'Enter your phone number',
+            suffixIcon: _controller.text.isEmpty ? null :	IconButton(
+              
+            onPressed:_clearTextField,
+            icon: const Icon(Icons.clear))
+            
+          ),
+          keyboardType: TextInputType.phone,
+          autofocus: true,
+          inputFormatters: [ 
+          
+            PhoneNumberFormatter(),
+            FilteringTextInputFormatter.allow(RegExp(r'^[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$')),
+            LengthLimitingTextInputFormatter(14)
+
+          ],
+      ),
+      const SizedBox(height: 20,),
+     
+      Align(
+        alignment: Alignment.centerRight,
+        child: ElevatedButton(
+          onPressed: _controller.text.length ==14 ? (){ 
+           showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+         
+          content: Text(_controller.text),
+        );
+      },
+   
+        );
+          } : null,
+          
+          
+          style:ElevatedButton.styleFrom(shape: const CircleBorder(),padding: const EdgeInsets.all(15)),
+          
+          child: const Icon(Icons.arrow_right_alt,),
+          ),
+      )
+      ],
+    
+          ),
+        )
+      ),
+
+      
+    );
+  }
+}
+
+class PhoneNumberFormatter extends TextInputFormatter {
+  
+ 
+  @override
+  TextEditingValue formatEditUpdate(
+    TextEditingValue oldValue,
+    TextEditingValue newValue,
+  ) {
+     
+    
+ 
+    if(oldValue.text.length>newValue.text.length){
+      return TextEditingValue(
+        text: newValue.text.toString(),
+        selection: TextSelection.collapsed(offset: newValue.text.length),
+      );
+    }
+ 
+    var newText = newValue.text;
+    if (newText.length == 1) newText ="(" + newText;
+    if (newText.length == 4) newText = newText + ") ";
+    if (newText.length == 9) newText = newText + "-";
+ 
+    return TextEditingValue(
+      text: newText.toString(),
+      selection: TextSelection.collapsed(offset: newText.length),
+    );
+  }
+}
+
